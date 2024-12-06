5b27604980cc5413e720eb0962957a308c04d47e
danielfdez17 <daniel17dev@gmail.com>
2024-12-06 10:47:31 +0100

    readBooking done

--- src/main/java/tfg/hotelmta/business/booking/BookingASImp.java
+++ src/main/java/tfg/hotelmta/business/booking/BookingASImp.java
@@ -1,5 +1,11 @@
 package tfg.hotelmta.business.booking;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.LockModeType;
+import tfg.hotelmta.integration.SingletonEntityManager;
+
 public class BookingASImp implements BookingAS {
 
     @Override
@@ -19,7 +25,27 @@ public class BookingASImp implements BookingAS {
 
     @Override
     public BookingDTO readBooking(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        BookingDTO bookingDTO = null;
+        EntityManagerFactory emf = SingletonEntityManager.getInstance();
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        try {
+            Booking booking = em.find(Booking.class, id, LockModeType.OPTIMISTIC);
+            if (booking == null) {
+                et.rollback();
+                throw new Exception("Booking with id " + id + " does not exist");
+            }
+
+            bookingDTO = booking.toTransfer();
+            et.commit();
+
+        } catch (Exception e) {
+            bookingDTO = null;
+        } finally {
+            em.close();
+        }
+        return bookingDTO;
     }
 
 }
