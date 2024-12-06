8073afee02be849fd293271b12b3a18e7eb902e1
danielfdez17 <daniel17dev@gmail.com>
2024-12-06 10:44:26 +0100

    readRoom done

--- src/main/java/tfg/hotelmta/business/room/RoomASImp.java
+++ src/main/java/tfg/hotelmta/business/room/RoomASImp.java
@@ -1,9 +1,35 @@
 package tfg.hotelmta.business.room;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.LockModeType;
+import tfg.hotelmta.integration.SingletonEntityManager;
+
 public class RoomASImp implements RoomAS {
 
     @Override
     public RoomDTO readRoom(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        EntityManagerFactory emf = SingletonEntityManager.getInstance();
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        RoomDTO roomDTO = null;
+        et.begin();
+        try {
+            Room room = em.find(Room.class, id, LockModeType.OPTIMISTIC);
+            if (room == null) {
+                et.rollback();
+                throw new Exception("Room with id " + id + " does not exist");
+            }
+
+            roomDTO = room.toTransfer();
+            et.commit();
+
+        } catch (Exception e) {
+            roomDTO = null;
+        } finally {
+            em.close();
+        }
+        return roomDTO;
     }
 }
