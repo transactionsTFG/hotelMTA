4d0a97c34bc489fa90e98389b97c082127b9710a
danielfdez17 <daniel17dev@gmail.com>
2024-12-06 11:20:08 +0100

    createBooking done + added validators

--- src/main/java/tfg/hotelmta/business/booking/BookingASImp.java
+++ src/main/java/tfg/hotelmta/business/booking/BookingASImp.java
@@ -4,18 +4,82 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.LockModeType;
+import tfg.hotelmta.business.customer.Customer;
+import tfg.hotelmta.business.validators.Validator;
 import tfg.hotelmta.integration.SingletonEntityManager;
 
 public class BookingASImp implements BookingAS {
 
     @Override
     public int createBooking(BookingDTO bookingDTO) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int res = -1;
+        if (this.isValid(bookingDTO)) {
+            EntityManagerFactory emf = SingletonEntityManager.getInstance();
+            EntityManager em = emf.createEntityManager();
+            EntityTransaction et = em.getTransaction();
+            et.begin();
+            try {
+                Customer customer = em.find(Customer.class, bookingDTO.getCustomerId(), LockModeType.OPTIMISTIC_FORCE_INCREMENT);
+                if (customer == null) {
+                    res = -1;
+                    throw new Exception("Non existent customer");
+                }
+
+                if (!customer.isActive()) {
+                    res = -1;
+                    throw new Exception("Non active customer");
+                }
+
+                Booking booking = new Booking(bookingDTO);
+                booking.setCustomer(customer);
+                em.persist(booking);
+                et.commit();
+                res = booking.getId();
+                bookingDTO.setId(res);
+
+            } catch (Exception e) {
+                et.rollback();
+            } finally {
+                em.close();
+            }
+        }
+        return res;
     }
 
     @Override
     public int updateBooking(BookingDTO bookingDTO) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int res = -1;
+        if (this.isValid(bookingDTO)) {
+            EntityManagerFactory emf = SingletonEntityManager.getInstance();
+            EntityManager em = emf.createEntityManager();
+            EntityTransaction et = em.getTransaction();
+            et.begin();
+            try {
+                Customer customer = em.find(Customer.class, bookingDTO.getCustomerId(), LockModeType.OPTIMISTIC_FORCE_INCREMENT);
+                if (customer == null) {
+                    res = -1;
+                    throw new Exception("Non existent customer");
+                }
+
+                if (!customer.isActive()) {
+                    res = -1;
+                    throw new Exception("Non active customer");
+                }
+
+                Booking booking = new Booking(bookingDTO);
+                booking.setCustomer(customer);
+                em.persist(booking);
+                et.commit();
+                res = booking.getId();
+                bookingDTO.setId(res);
+
+            } catch (Exception e) {
+                et.rollback();
+            } finally {
+                em.close();
+            }
+        }
+        return res;
     }
 
     @Override
@@ -46,6 +110,16 @@ public class BookingASImp implements BookingAS {
             em.close();
         }
         return bookingDTO;
+    }
+
+    private boolean isValid(BookingDTO booking) {
+        return booking != null
+                && Validator.isDate(booking.getDate())
+                && !booking.getAgencyName().isBlank()
+                && !booking.getAgencyName().isEmpty()
+                && booking.getCustomerId() > 0
+                && booking.getPeopleNumber() > 0
+                && !Validator.hasSQLInjection(booking.getAgencyName());
     }
 
 }
---
+++ src/main/java/tfg/hotelmta/business/validators/Validator.java
@@ -0,0 +1,17 @@
+package tfg.hotelmta.business.validators;
+
+public class Validator {
+
+    // DD-MM-YYYY
+    public static boolean isDate(String date) {
+        if (date.isBlank() || date.isEmpty()) {
+            return false;
+        }
+
+        return date.matches("\\d{2}-\\d{2}-\\d{4}");
+    }
+    
+    public static boolean hasSQLInjection(String str) {
+        return !str.matches("");
+    }
+}
