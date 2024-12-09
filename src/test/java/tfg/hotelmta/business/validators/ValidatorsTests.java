cab17f497ced36d34ea5e13d7e573571968e5453
danielfdez17 <daniel17dev@gmail.com>
2024-12-09 15:23:55 +0100

    preparing validators tests

--- src/test/java/tfg/hotelmta/business/validators/ValidatorsTests.java
+++ src/test/java/tfg/hotelmta/business/validators/ValidatorsTests.java
@@ -3,10 +3,14 @@ package tfg.hotelmta.business.validators;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class ValidatorsTest {
+public class ValidatorsTests {
 
     @Test public void testIsDate() {
         Assert.assertTrue("", Validator.isDate("01-01-2025"));
     }
     
+    @Test public void testIsEmail() {}
+    @Test public void testIsPhone(){}
+    @Test public void testIsDni(){}
+    @Test public void testHasSQLInjection(){}
 }
