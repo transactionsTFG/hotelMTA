52a6b8ccb2996f24f5fc8409cd40a2fa35897290
danielfdez17 <daniel17dev@gmail.com>
2024-12-14 11:57:04 +0100

    CustomerASImp updated to return Result<?>

--- src/main/java/business/customer/CustomerASImp.java
+++ src/main/java/business/customer/CustomerASImp.java
@@ -1,14 +1,21 @@
 package business.customer;
 
+import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.LockModeType;
 import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import business.utils.ErrorResponses;
+import common.consts.ASError;
+import common.dto.result.Result;
+import common.exception.ASException;
+import common.exception.CustomerASException;
+import common.validators.Validator;
 import integration.transaction.Transaction;
 import integration.transaction.TransactionManager;
 
+@Stateless
 public class CustomerASImp implements CustomerAS {
 
     private final EntityManager em;
@@ -20,102 +27,76 @@ public class CustomerASImp implements CustomerAS {
 
     @Override
     @Transactional
-    public int createCustomer(CustomerDTO customerDTO) {
-        int res = ErrorResponses.SYNTAX_ERROR;
+    public Result<CustomerDTO> createCustomer(CustomerDTO customerDTO) throws ASException {
 
-        if (!this.isValid(customerDTO)) {
-            return res;
+        this.isValid(customerDTO);
+
+        TypedQuery<Customer> query = em.createNamedQuery("business.customer.getByDni", Customer.class);
+        query.setParameter("dni", customerDTO.getDni());
+        Customer customer = query.getResultList().isEmpty() ? null : query.getResultList().getFirst();
+
+        if (customer == null) {
+            customer = new Customer(customerDTO);
+            em.persist(customer);
+            return Result.success(customer.toDTO());
         }
 
-        Transaction t = TransactionManager.getInstance().newTransaccion();
-        t.start();
-        EntityManager em = (EntityManager) t.getResource();
-
-        try {
-            TypedQuery<Customer> query = em.createNamedQuery("business.customer.getByDni", Customer.class);
-            query.setParameter("dni", customerDTO.getDni());
-            Customer customer = query.getResultList().isEmpty() ? null : query.getResultList().getFirst();
-            if (customer == null) {
-                customer = new Customer(customerDTO);
-                em.persist(customer);
-                t.commit();
-                res = customer.getId();
-                customerDTO.setId(res);
-            } else {
-                if (customer.isActive()) {
-                    res = ErrorResponses.ACTIVE_CUSTOMER;
-                    throw new ASException("Customer already active");
-                } else {
-                    customer.setName(customerDTO.getName());
-                    customer.setEmail(customerDTO.getEmail());
-                    customer.setPhone(customerDTO.getPhone());
-                    customer.setDni(customerDTO.getDni());
-                    customer.setActive(true);
-                    t.commit();
-                    res = ErrorResponses.NON_ACTIVE_CUSTOMER;
-                    customerDTO.setId(customer.getId());
-                }
-            }
-        } catch (Exception e) {
-            t.rollback();
+        if (customer.isActive()) {
+            throw new CustomerASException(ASError.ACTIVE_CUSTOMER);
         }
 
-        return res;
+        customer.setName(customerDTO.getName());
+        customer.setEmail(customerDTO.getEmail());
+        customer.setPhone(customerDTO.getPhone());
+        customer.setDni(customerDTO.getDni());
+        customer.setActive(true);
+        em.persist(customer);
+        return Result.success(customer.toDTO());
+
     }
 
     @Override
     @Transactional
-    public CustomerDTO readCustomer(int id) {
-        CustomerDTO customerDTO = null;
-        Transaction t = TransactionManager.getInstance().newTransaccion();
-        t.start();
-        EntityManager em = (EntityManager) t.getResource();
-        try {
-            Customer customer = em.find(Customer.class, id, LockModeType.OPTIMISTIC);
-            if (customer == null) {
-                throw new ASException("Customer with id " + id + " does not exist");
-            }
-            t.commit();
-            customerDTO = customer.toDTO();
-        } catch (Exception e) {
-            t.rollback();
+    public Result<CustomerDTO> readCustomer(int id) throws ASException {
+
+        Customer customer = em.find(Customer.class, id, LockModeType.OPTIMISTIC);
+
+        if (customer == null) {
+            throw new CustomerASException(ASError.CUSTOMER_NOT_FOUND);
         }
-        return customerDTO;
+
+        return Result.success(customer.toDTO());
+
     }
 
     @Override
     @Transactional
-    public int deleteCustomer(int id) {
-        int res = ErrorResponses.NON_EXISTENT_CUSTOMER;
-        Transaction t = TransactionManager.getInstance().newTransaccion();
-        t.start();
-        EntityManager em = (EntityManager) t.getResource();
-        try {
-            Customer customer = em.find(Customer.class, id);
-            if (customer == null) {
-                throw new ASException("Non existent customer");
-            }
+    public Result<Void> deleteCustomer(int id) throws ASException {
 
-            if (!customer.isActive()) {
-                res = ErrorResponses.NON_ACTIVE_CUSTOMER;
-                throw new ASException("Non active customer");
-            }
+        Customer customer = em.find(Customer.class, id);
 
-            customer.setActive(false);
-            t.commit();
-            res = id;
-
-        } catch (Exception e) {
-            if (!(e instanceof ASException)) {
-                res = ErrorResponses.UNEXPECTED_ERROR;
-            }
-            t.rollback();
+        if (customer == null) {
+            throw new CustomerASException(ASError.CUSTOMER_NOT_FOUND);
         }
-        return res;
+
+        if (!customer.isActive()) {
+            throw new CustomerASException(ASError.NON_ACTIVE_CUSTOMER);
+        }
+
+        customer.setActive(false);
+        return Result.success(null);
+
     }
 
-    private boolean isValid(CustomerDTO customer) {
-        return true;
+    private void isValid(CustomerDTO customer) throws ASException {
+        if (customer == null)
+            throw new ASException(ASError.NULL_CUSTOMER);
+        if (!Validator.isDni(customer.getDni()))
+            throw new ASException(ASError.INVALID_DNI);
+        if (!Validator.isEmail(customer.getEmail()))
+            throw new ASException(ASError.INVALID_EMAIL);
+        if (!Validator.isPhone(customer.getPhone()))
+            throw new ASException(ASError.INVALID_PHONE);
     }
 
 }
---
+++ src/main/java/common/exception/CustomerASException.java
@@ -0,0 +1,9 @@
+package common.exception;
+
+public class CustomerASException extends ASException {
+    private static final long serialVersionUID = -3244080412307055345L;
+
+    public CustomerASException(final String message) {
+        super(message);
+    }
+}
